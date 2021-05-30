6f5db649d74da3955a068b34a67e1725645843dd
oleg086rus <o.pototskiy@yandex.ru>
2021-05-31 00:12:36 +0500

    заглох на 2 задании при добавлении кнопки

---
+++ hw4/task1/index.php
@@ -0,0 +1,56 @@
+<?php
+$images = [
+    '01.jpg',
+    '02.jpg',
+    '03.jpg',
+    '04.jpg',
+    '05.jpg',
+    '06.jpg',
+    '07.jpg',
+    '08.jpg',
+    '09.jpg',
+    '10.jpg',
+    '11.jpg',
+    '12.jpg',
+    '13.jpg',
+    '14.jpg',
+    '15.jpg'
+];
+//var_dump($images);
+//die();
+?>
+
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+<title>Моя галерея</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<script type="text/javascript" src="./scripts/jquery-1.4.3.min.js"></script>
+<script type="text/javascript" src="./scripts/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+<script type="text/javascript" src="./scripts/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<link rel="stylesheet" type="text/css" href="./scripts/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("a.photo").fancybox({
+			transitionIn: 'elastic',
+			transitionOut: 'elastic',
+			speedIn: 500,
+			speedOut: 500,
+			hideOnOverlayClick: false,
+			titlePosition: 'over'
+		});	}); </script>
+
+</head>
+
+<body>
+<div id="main">
+<div class="post_title"><h2>Моя галерея</h2></div>
+	<div class="gallery">
+<?php foreach ($images as $filename): ?>
+<a rel="gallery" class="photo" href="/gallery_img/big/<?=$filename?>"><img src="/gallery_img/small/<?=$filename?>" width="150" height="100" /></a>
+<?php endforeach; ?>
+</div>
+</div>
+
+</body>
+</html>
