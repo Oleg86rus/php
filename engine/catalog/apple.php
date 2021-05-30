13caee1cb6a0394f0e7b61c6ba8a557cd0d67508
oleg086rus <o.pototskiy@yandex.ru>
2021-05-30 14:53:24 +0500

    hw3

---
+++ engine/catalog/apple.php
@@ -0,0 +1,11 @@
+<h2>Каталог</h2>
+<?php var_dump($catalog)?>
+<div>
+    <div>
+        <h2>Яблоко</h2>
+        <img src="/img/apple.jpg" width="150"><br>
+        Цена: 12<br>
+        <button>Купить</button>
+        <hr>
+    </div>
+</div>
